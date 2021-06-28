8429786584fcb98f9a4b0ff4d4e973e3d64db1d2
Nedas Surkus <nedassurkus@gmail.com>
2021-06-28 22:11:19 +0200

    Added work experience

--- src/app/work-examples/work-examples.component.ts
+++ src/app/work-examples/work-examples.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faCalendar, faLightbulb } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-work-examples',
   templateUrl: './work-examples.component.html',
   styleUrls: ['./work-examples.component.scss']
 })
 export class WorkExamplesComponent implements OnInit {
+
+  faCalendar = faCalendar;
+  faLightbulb = faLightbulb;
 
   constructor() { }
 
