2b014ccac8daf4ef9dcc0ecba76bdae484fb4af4
Nedas Surkus <nedassurkus@gmail.com>
2022-02-09 23:54:39 +0100

    Added new experience

--- src/app/about-me/skill-set/skill-set.component.ts
+++ src/app/about-me/skill-set/skill-set.component.ts
@@ -9,62 +9,56 @@ import {interval} from "rxjs";
 export class SkillSetComponent implements OnInit {
 
   htmlValue = 0;
-  trueHTMLValue = 75 / 200;
+  trueHTMLValue = 75;
 
   CSSValue = 0;
-  trueCSSValue = 70 / 200;
+  trueCSSValue = 70;
 
   JSValue = 0;
-  trueJSValue = 70 / 200;
+  trueJSValue = 70;
 
   AngularValue = 0;
-  trueAngularValue = 80 / 200;
+  trueAngularValue = 80;
 
   JavaValue = 0;
-  trueJavaValue = 80 / 200;
+  trueJavaValue = 80;
 
   CValue = 0;
-  trueCValue = 75 / 200;
+  trueCValue = 75;
 
   NodeValue = 0;
-  trueNodeValue = 60 / 200;
+  trueNodeValue = 60;
 
   SQLValue = 0;
-  trueSQLValue = 90 / 200;
+  trueSQLValue = 90;
 
   FirebaseValue = 0;
-  trueFirebaseValue = 90 / 200;
+  trueFirebaseValue = 90 ;
 
   curSec = 0;
 
   constructor() {
-    this.startTimer(200);
+      this.startTime();
   }
 
   ngOnInit(): void {
 
   }
 
-  startTimer(miliseconds: number) {
-    const time = miliseconds;
-    const timer$ = interval(1);
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
 
-    const sub = timer$.subscribe((sec) => {
-      this.htmlValue = this.htmlValue + this.trueHTMLValue;
-      this.CSSValue = this.CSSValue + this.trueCSSValue;
-      this.JSValue = this.JSValue + this.trueJSValue;
-      this.AngularValue = this.AngularValue + this.trueAngularValue;
-      this.JavaValue = this.JavaValue + this.trueJavaValue;
-      this.CValue = this.CValue + this.trueCValue;
-      this.NodeValue = this.NodeValue + this.trueNodeValue;
-      this.SQLValue = this.SQLValue + this.trueSQLValue;
-      this.FirebaseValue = this.FirebaseValue + this.trueFirebaseValue;
-
-      this.curSec = sec;
-
-      if (this.curSec === miliseconds) {
-        sub.unsubscribe();
-      }
-    });
+  async startTime(){
+    await this.delay(200);
+    this.htmlValue = this.htmlValue + this.trueHTMLValue;
+    this.CSSValue = this.CSSValue + this.trueCSSValue;
+    this.JSValue = this.JSValue + this.trueJSValue;
+    this.AngularValue = this.AngularValue + this.trueAngularValue;
+    this.JavaValue = this.JavaValue + this.trueJavaValue;
+    this.CValue = this.CValue + this.trueCValue;
+    this.NodeValue = this.NodeValue + this.trueNodeValue;
+    this.SQLValue = this.SQLValue + this.trueSQLValue;
+    this.FirebaseValue = this.FirebaseValue + this.trueFirebaseValue;
   }
 }
